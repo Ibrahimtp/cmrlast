e2413c8f3af49b4331e69b3c06d8ea33682ab24d
ibrahim130860011@gmail.com <ibrahim130860011@gmail.com>
2022-10-02 11:28:12 +0300

    finished implementing controller for modify route

--- routes/question.js
+++ routes/question.js
@@ -16,5 +16,15 @@ Router.get("/allquestions", QuestionController.allquestions);
 Router.get("/questiondetail/:id/:uid", QuestionController.questiondetail);
 
 //modify
+Router.get("/modify", QuestionController.modificationPage);
+
+//Edit
+Router.get("/modify/edit/:qid", QuestionController.modificationEdit);
+
+//delete
+Router.get("/modify/delete/:id", QuestionController.modificationDelete);
+
+//update
+Router.post("modify/update/:id", QuestionController.modificationUpdate);
 
 module.exports = Router;
